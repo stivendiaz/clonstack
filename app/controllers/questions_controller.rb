c44438c334a970c367054bed92bf4dce90e1841a
Stiven Diaz <stivendiazagudelo@gmail.com>
2018-03-30 21:01:06 -0500

    Se pueden hacer preguntas con sus respectivas respuestas y comentarios

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,44 @@
 class QuestionsController < ApplicationController
+    def new
+        @question =Question.new
+      end
+      def index
+        @questions =Question.all
+      end
+      def create
+        @question =Question.create(questions_params)
+        @question[:user_id] = current_user[:id]
+        @question.save
+        redirect_to questions_path(@question)
+        
+      end
+      def show
+        @question  =Question.find(params[:id])
+        @answer = Answer.new
+        @comment = Comment.new
+      end
+    
+      def destroy
+        question =Question.find(params[:id])
+        question.destroy
+      
+        redirect_to questions_path
+      end
+    
+      def edit
+        @question  =Question.find(params[:id])
+      end
+    
+      def update
+        @question =Question.find(params[:id])
+        if @question.update(question_params)
+          redirect_to question_path, notice: "Question reload!"
+        else
+          render :edit
+        end
+      end
+        private
+        def questions_params
+          params.require(:question).permit(:title,:body)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root 'questions#index'
 
   resources :questions
+  resources :answers
+  resources :comments
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
